eb56fe87186685ce65212bb15902eef845c140dc
JopsonDev <jopsonmason@outlook.com>
2026-04-16 15:51:37 -0400

    clean up and fix for median

--- src/main/java/com/pluarlsight/TestStatistics.java
+++ src/main/java/com/pluarlsight/TestStatistics.java
@@ -5,30 +5,34 @@ import java.util.Arrays;
 public class TestStatistics {
     static void main() {
         int[] scores = {90, 98, 95, 69, 80, 78, 74, 40, 55, 100};
-        int average = 0;
-        for (int i = 0; i < 10; i++){
-            average += scores[i];
-        }
-        average /= scores.length;
-        System.out.println(average);
 
+        int sum = 0;
         int max = 0;
-
-        for (int maxScore: scores) {
-            if (maxScore > max){
-                max = maxScore;
-            }
-        }
-        System.out.println(max);
-
         int min = scores[0];
 
-        for (int minScore : scores){
-            if(minScore < min) {
-                min = minScore;
+        for (int score : scores){
+            sum += score;
+            if(score < min) {
+                min = score;
+            }
+            if (score > max){
+                max = score;
             }
         }
+        double average = (double)sum / scores.length;
+        System.out.println(average);
+        System.out.println(max);
         System.out.println(min);
+
+        Arrays.sort(scores);
+        int middle = scores.length / 2;
+        if (scores.length % 2 == 0) {
+            System.out.println(scores[middle]);
+            System.out.println(middle);
+        } else {
+            middle = (scores[middle] + scores[(middle - 1)]) / 2;
+            System.out.println(middle);
+        }
 
     }
 }
